b162f821830a66dabc4a7bd1b2311fad82d1ae2c
Evan Sosenko <razorx@evansosenko.com>
2015-12-06 22:58:02 -0800

    Up to Ch. 3: Data Structures

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,63 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+
+(+ 1 2 3)
+
+(str "I " "am " "brave.")
+
+(if true
+  "Puppies"
+  "No Puppies")
+
+(if false
+  "Puppies"
+  "No Puppies")
+
+(if false
+  "No Puppies")
+
+(if true
+  (do (println "Success!")
+      "Puppies")
+  (do (println "Failure!")
+      "No Puppies"))
+
+(when true
+  (println "Success!")
+  "Puppies")
+
+(nil? 1)
+(nil? nil?)
+
+(if 0
+  "Puppies")
+
+(if nil
+  "Puppies"
+  "No Puppies")
+
+(= 1 1)
+(= nil nil)
+(= 1 2)
+
+(or false nil :puppies :kitties)
+(or (= 0 1) (= "yes" "no"))
+(or nil)
+
+(and :kitties :puppies)
+(and :puppies nil false)
+
+(def tools-to-use
+  ["vim", "zsh", "tmux", "git"])
+tools-to-use
+
+(defn error-message
+  [severity]
+  (str "Oh noes! "
+       (if (= severity :cuddles)
+         "Too many puppies!"
+         "No puppies!")))
+
+(error-message :cuddles)
+(error-message :itsbad)
