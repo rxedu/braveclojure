ffb88b31c7a0a1970a86f9154b2f4a7eb2cfbddf
Evan Sosenko <razorx@evansosenko.com>
2015-12-10 13:46:54 -0800

    Up to Ch. 4 The Collection Abstraction

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -251,3 +251,37 @@ tools-to-use
             new-map))
         {}
         {:a 5 :b 2})
+
+(take 3 [1 2 3 4 5])
+(drop 3 [1 2 3 4 5])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+(drop-while #(< (:month %) 3) food-journal)
+(take-while #(< (:month %) 4) (drop-while #(< (:month %) 2) food-journal))
+(filter #(< (:human %) 5) food-journal)
+(filter #(< (:month %) 3) food-journal)
+(some #(> (:critter %) 5) food-journal)
+(some #(> (:critter %) 3) food-journal)
+(some #(and (> (:critter %) 3) %) food-journal)
+
+(sort [4 3 5 3 2])
+(sort-by count ["aaa" "c" "cc"])
+(concat [1 2] [3 4])
+
+(concat (take 8 (repeat "na")) ["Batman!"])
+(take 3 (repeatedly #(rand-int 10)))
+
+(defn even-numbers
+  ([] (even-numbers 0))
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+(take 10 (even-numbers))
